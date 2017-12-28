78880a7ce500fe428c6529d0e73e47636160966a
Waldemar Figueroa <waldemar@boalt.com>
2017-12-28 15:39:26 -0500

    Fix incorrect variable with non-replaced subdomain

--- src/templates/laroute.js
+++ src/templates/laroute.js
@@ -33,7 +33,7 @@
                 var qs = this.getRouteQueryString(parameters);
 
                 if (this.absolute && this.isOtherHost(route)){
-                    return "//" + route.host + uri + qs;
+                    return "//" + host + uri + qs;
                 }
 
                 return this.getCorrectUrl(uri + qs);
